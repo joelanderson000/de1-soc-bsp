6dec0d6f9eed0ceb706aa38fb1ec6d8798da3796
Joel Anderson <saskaperture@gmail.com>
2026-02-15 11:40:47 -0800

    add bsp generator scripts from u-boot

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Configuring environment variables for Yocto build system..."
+
+CWD=$(pwd)
+
+# Set Versions
+export LINUX_VER=6.12.43
+export UBOOT_VER=v2025.01
+
+export MACHINE="de1-soc"
+
+# Set Linux Version
+echo "LINUX_VERSION        = $LINUX_VER"
+LINUX_SOCFPGA_BRANCH=socfpga-$LINUX_VER-lts
+echo "LINUX_SOCFPGA_BRANCH = $LINUX_SOCFPGA_BRANCH"
+
+# Set U-Boot Version
+export UBOOT_REL=
+echo "UBOOT_VERSION        = $UBOOT_VER$UBOOT_REL"
+UBOOT_SOCFPGA_BRANCH=socfpga_$UBOOT_VER$UBOOT_REL
+echo "UBOOT_SOCFPGA_BRANCH = $UBOOT_SOCFPGA_BRANCH"
+
+. layers/openembedded-core/oe-init-build-env
